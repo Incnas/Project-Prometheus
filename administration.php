1d50c767dce6b77d32bc6b323ad0140c61be753b
Incnas <duootherduo@yahoo.com.au>
2013-10-09 21:20:26 +1100

    Calender Updates

--- administration.php
+++ administration.php
@@ -17,11 +17,7 @@
 <h3>Upload Teacher Classes</h3>
 <h3>Upload Calender</h3>
 <h4>Edit Calender</h4>
-<form action="">
-	<input type="checkbox" name="weekends" value="weekEnds">Display Weekends<br>
-</form>
-<p>Option Display Calender</p>
-<p>Display Calender Here</p>
+<button class="delete">Clear Calender</button>
 <button class="new">Add Week</button>
 <div class='datagrid'>
 	<table>
@@ -38,30 +34,59 @@
 	<tbody>
 	<?
 		while($stmt->fetch()){
+		
 	?>
 			<tr>
-				<td><button class="day"><?=$row['name'];?></button></td>
+				<td><button class="day">Week <?=$row['week_num'];?></button></td>
 			</tr>	
 	<?
 		}
 	?>
+
+	</tr>
 	</tbody>
 	</table>
 </div>
-
-<p>Add New Week</p>
-
 
 <?
 	include($_SERVER['DOCUMENT_ROOT'].'/includes/footer.inc.php');
 ?>
 <div class="new_week"></div>
+<div class="clear_calender">Are you sure? This move is irreversable.</div>
 <script>
-// New Assessment Button
+$(function(){
+//Clear Calender Button	
+	$('button.delete').button({
+		icons:{primary:"ui-icon-trash"}
+	}).click(function(){
+		$('div.clear_calender').dialog('open').attr('id', $(this).attr('id'));
+	})
+	$('div.clear_calender').dialog({
+		autoOpen: false,
+		height: 200,
+		width: 350,
+		modal: true,
+		resizable: false,
+		draggable: false,
+		title: "Clear Calender?",
+		buttons: {
+			"Delete": function(){
+				var path = "/ajax/clear_calender.php?id="+$(this).attr('id');
+				$.get(path, function(){
+					location.href = location.href;
+				})
+			},
+			"Cancel": function(){
+				$(this).dialog('close');
+			}
+		}
+	})
+
+// New Week Button
 	$('button.new').button({
 		icons :{primary: "ui-icon-plusthick"}
 	}).click(function(){
-		var path = '/ajax/new_week.php?unit_code='+$(this).attr('id');
+		var path = '/ajax/new_week.php';
 		$('div.new_week').load(path).dialog('open');
 	})
 	
--- ajax/new_week.php
+++ ajax/new_week.php
@@ -17,21 +17,21 @@ else {
 <form name="new_week">
 	<table>
 		<tr>
-			<td>Name:</td>
+			<td>Week Number:</td>
 			<td><input type="text" size="30" name="name" /></td>
 		</tr>
 		<tr>
 			<td>Start Date:</td>
-			<td><input type="week" size="30" name="start_date" /></td>
+			<td><input type="date" size="30" name="start_date" /></td>
 		</tr>
 
 		<tr>
-			<td>Out Date:</td>
-			<td><input type="text" size="30" name="out_date" /></td>
+			<td>School Week</td>
+			<td><input type="checkbox" size="30" name="out_date" /></td>
 		</tr>
 		<tr>
-			<td>Due Date:</td>
-			<td><input type="text" size="30" name="due_date" /></td>
+			<td>Notes:</td>
+			<td><input type="text" size="30" name="notes" /></td>
 		</tr>
 	</table>
 
